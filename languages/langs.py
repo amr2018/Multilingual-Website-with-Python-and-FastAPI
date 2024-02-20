d2fb9b2405c0931da6da871d2b9e0d523e7df5a9
Free Python Code <71100436+amr2018@users.noreply.github.com>
2024-02-20 08:17:54 +0200

    Update langs.py
    solve encoding error
    make it work with many pages not one page

--- languages/langs.py
+++ languages/langs.py
@@ -2,6 +2,16 @@
 import json
 
 def load_lang_data(page_name, lang = 'en'):
-    languages = json.load(open(f'languages/{lang}.json', 'r'))
-    return languages['web_pages'][0][page_name]
-
+    languages = json.load(open(
+            f'languages/{lang}.json',
+            'r', 
+            errors='ignore',
+            encoding='utf-8'
+        )
+    )
+    
+    for i in range(len(languages['web_pages'])):
+        try:
+            return languages['web_pages'][i][page_name]
+        except:
+            pass
